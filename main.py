361ac14625fa115355fb8dca498a658bfccda02b
jackson147 <jaswahhihi@gmail.com>
2023-03-09 07:57:32 -0800

    Modified app to now work with the selinum webdriver. This gets around issues where a page needs to intially load before showing content. App now searchs for text and email if there's a change to that text

--- main.py
+++ main.py
@@ -8,17 +8,34 @@ import sys
 import traceback
 import smtplib
 
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# Global vars
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
 CONFIGS_LOCATION = None
 CONFIG_FILE_PATH = None
-
 CHECK_URL = None
+SEARCH_TEXT = None
+PAGE_LOADING_TIME = 0
 EMAIL_USERNAME = None
 EMAIL_PASSWORD = None
 RECIPIENT_ADDRESS = None
 CHECK_INTERVAL_SECONDS = None
 
-def main():
+# Selenium webdriver setup
+options = Options()
+options.add_argument(
+    f'user-agent={USER_AGENT}')
+options.add_argument("no-sandbox")
+options.add_argument("disable-dev-shm-usage")
+options.add_argument("--headless")
 
+driver = webdriver.Chrome(
+    options=options)
+
+def main():
     global CONFIGS_LOCATION
     global CONFIG_FILE_PATH
 
@@ -39,42 +56,31 @@ def main():
 
     load_config(CONFIG_FILE_PATH)
 
-    if CHECK_URL is None or EMAIL_USERNAME is None or EMAIL_PASSWORD is None or EMAIL_USERNAME is None or RECIPIENT_ADDRESS is None:
+    if CHECK_URL is None or SEARCH_TEXT is None or EMAIL_USERNAME is None or EMAIL_PASSWORD is None or EMAIL_USERNAME is None or RECIPIENT_ADDRESS is None:
         exit_with_failure(
             'missing required credentials in environment variables')
-        
-    # URL to monitor
-    url = Request(CHECK_URL,
-                headers={'User-Agent': 'Mozilla/5.0'})
     
     # Begin the loop to check if site has changed.
-    response = urlopen(url).read()
-    currentHash = hashlib.sha224(response).hexdigest()
-    print_with_timestamp("Inital hash:", currentHash)
     while True:
         try:
-            # Wait for check interval
-            time.sleep(CHECK_INTERVAL_SECONDS)
-
-            print("Checking url", CHECK_URL)
-            # Perform the get request
-            response = urlopen(url).read()
-            # Create a new hash
-            newHash = hashlib.sha224(response).hexdigest()
-
-            # Check if new hash is same as the previous hash
-            if newHash == currentHash:
-                print_with_timestamp("No change, sleeping for ", CHECK_INTERVAL_SECONDS, "seconds")
-                continue
-
-            # If something changed, alert and set currentHash
-            else:
+            print_with_timestamp("Checking url {} for text: '{}'".format(CHECK_URL, SEARCH_TEXT))
+            driver.get(CHECK_URL)
+            if(PAGE_LOADING_TIME > 0):
+                print_with_timestamp("Sleeping for {}s while page loads...".format(PAGE_LOADING_TIME))
+                time.sleep(PAGE_LOADING_TIME)
+            html = driver.page_source
+            soup = BeautifulSoup(html, features="html.parser")
+            value = soup.find(string=SEARCH_TEXT)
+            
+            if value is None or value == "":
                 # notify
-                print_with_timestamp("Site changed, sending alert")
+                print_with_timestamp("Search text no longer exists on site, sending alert!".format(SEARCH_TEXT))
                 send_changed_alert()
-
-                currentHash = newHash
-                print_with_timestamp("New hash:", currentHash)
+                exit(0)
+                
+            # Wait for check interval                
+            print_with_timestamp("No change, sleeping for {} seconds".format(CHECK_INTERVAL_SECONDS))
+            time.sleep(CHECK_INTERVAL_SECONDS)
 
         # To handle exceptions
         except Exception as e:
@@ -93,6 +99,8 @@ def exit_with_failure(message):
 
 def load_config(path):
     global CHECK_URL
+    global SEARCH_TEXT
+    global PAGE_LOADING_TIME
     global EMAIL_USERNAME
     global EMAIL_PASSWORD
     global RECIPIENT_ADDRESS
@@ -102,6 +110,8 @@ def load_config(path):
         data = json.load(file)
 
         CHECK_URL = data['CHECK_URL']
+        SEARCH_TEXT = data['SEARCH_TEXT']
+        PAGE_LOADING_TIME = int(data['PAGE_LOADING_TIME'])
         EMAIL_USERNAME = data['EMAIL_USERNAME']
         EMAIL_PASSWORD = data['EMAIL_PASSWORD']
         CHECK_INTERVAL_SECONDS = int(data['CHECK_INTERVAL_SECONDS'])
